d8fb162d869654db1693257efde3f53aebfbc72e
vanessa ching <102451117+vanessaching@users.noreply.github.com>
2026-07-26 13:14:18 -0400

    Add: valid-sudoku - submission-5

---
+++ Data Structures & Algorithms/valid-sudoku/submission-5.py
@@ -0,0 +1,25 @@
+# an explanation from the video i watched so i can understand it better
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # creates sets so that all values are unique
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
+
+        # fixed value because 9 x 9 
+        for r in range(9):
+            for c in range(9):
+                # IMPORTANT
+                # almost forgot this 
+                if board[r][c] == ".":
+                    continue
+                # checks if not unique to the three sets
+                # check WITHIN each row/col/square not ALL
+                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r//3, c//3)]:
+                    return False
+                # adds the values 
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                # use this tuple as the key since it covers boxes 0-2 using integer division! 
+                squares[(r//3, c//3)].add(board[r][c])
+        return True
